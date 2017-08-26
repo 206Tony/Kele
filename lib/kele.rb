527255d1e17e0ead13d1b28e2866c11b407c15f4
Anthony Zardis <anthonyzardis@gmail.com>
2017-08-26 14:42:25 -0700

    4-Mentor-Avail

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,28 +3,32 @@ require 'json'
 
 class Kele
   include HTTParty
-  #base_uri = 'https://www.bloc.io/api/v1'
 
 	def initialize(email, password)
-    response = self.class.post(api_url('sessions'), body: { "email": email, "password": password })
+    response = self.class.post(base_api_endpoint('sessions'), body: { "email": email, "password": password })
     raise "Invalid email or password" if response.code == 404
-    @auth_token = response['auth_token']
+    @auth_token = response["auth_token"]
   end
 
   def get_me
-    response = self.class.get(api_url('users/me'), headers: { 'authorization' => @auth_token })
+    response = self.class.get(base_api_endpoint('users/me'), headers: { 'authorization' => @auth_token })
     @user_data = JSON.parse(response.body)
-    # @user_data.keys.each do |key|
-    #   self.class.send(:define_method, key.to_sym) do
-    #     @user_data[key]
-    #   end
-    # end
+    @user_data.keys.each do |key|
+      self.class.send(:define_method, key.to_sym) do
+        @user_data[key]
+      end
+    end
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
   end
 
   private
 
-  def api_url(endpoint)
-    "https://www.bloc.io/api/v1/#{endpoint}"
+  def base_api_endpoint(end_point))
+    "https://www.bloc.io/api/v1/#{end_point}"
   end
 end
 
